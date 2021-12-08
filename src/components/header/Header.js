d061219e8f9feaf21369baf9d2a6ec7be168c3ad
WWWarren <49315386+WWWarren@users.noreply.github.com>
2021-12-08 11:59:21 +0000

    styled header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,27 @@
 import react from 'react';
 
+import logo from '../../images/logo.png'
+
 export const Header = () => {
     return (
-        <header className="">
+        <header className="pt-5 pb-5">
             <div className="container">
-                <div className="row justify-content-center" style={{ justifyContent: 'center' }}>
-                    <div className="col-2">Logo</div>
-                    <div className="col-6">Nav</div>
-                    <div className="col-2">More Buttons</div>
+                <div className="row justify-content-between align-items-center g-0">
+                    <div className="col-4 col-lg-2 logo"><img src={logo} alt="Oxford Instruments" /></div>
+                    <div className="col-6 nav justify-content-between">
+                        <div>Businesses <i class="fas fa-chevron-down" /></div>
+                        <div>Applications <i class="fas fa-chevron-down" /></div>
+                        <div>Products <i class="fas fa-chevron-down" /></div>
+                        <div>Latest <i class="fas fa-chevron-down" /></div>
+                        <div>Services <i class="fas fa-chevron-down" /></div>
+                        <div>Shop <i class="fas fa-chevron-down" /></div>
+                    </div>
+                    <div className="col-5 col-lg-2 more-buttons d-flex justify-content-between">
+                        <button className="mob-menu"><i class="fas fa-bars" /></button>
+                        <button><i class="fas fa-ellipsis-v" /></button>
+                        <button><i class="fas fa-globe-europe" /></button>
+                        <button><i class="fas fa-search" /></button>
+                    </div>
                 </div>
             </div>
         </header>
