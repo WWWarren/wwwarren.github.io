d199c8ceda1fe02bbf49f1034a6df17630fd0d52
WWWarren <49315386+WWWarren@users.noreply.github.com>
2021-12-14 17:02:59 +0000

    added some content to the about us section

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import { Header } from './header/Header';
 import { Hero } from './hero/Hero';
+import { AboutUs } from './aboutUs/AboutUs';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <AboutUs />
     </>
   );
 }
---
+++ src/components/aboutUs/AboutUs.js
@@ -0,0 +1,25 @@
+import react from 'react';
+
+export const AboutUs = () => {
+    return (
+        <div id="aboutUs" className="h-100 aboutUs">
+            <div className="container">
+                <div className="row">
+                    <div className="col-5 offset-2">Our core purpose is to support our customers to address some of the world's most pressing challenges, enabling a greener economy, increased connectivity, improved health and leaps in scientific understanding.</div>
+                    <div className="col-3"></div>
+                </div>
+                <div className="row">
+                    <div className="col-3 offset-2"></div>
+                    <div className="col-5">We are proud to be recognised as the leaders in what we do and for the difference we make in the world.</div>
+                </div>
+                <div className="row">
+                    <div className="col-5 offset-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu lacinia turpis. Sed dapibus sed nisl ut cursus. 
+                        Praesent arcu mauris, bibendum nec sapien consectetur, pretium pellentesque nisi. Proin efficitur interdum nulla, non varius nunc sollicitudin sit amet. Maecenas condimentum urna justo, in hendrerit erat varius sed.
+                    </div>
+                    <div className="col-3"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
