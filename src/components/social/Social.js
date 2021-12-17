af64bf3e03c8e622ebe4b13f4e12be230002957f
WWWarren <49315386+WWWarren@users.noreply.github.com>
2021-12-17 11:08:13 +0000

    added social and share sections to page

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -1,7 +1,37 @@
 import react from 'react';
 
+import social1 from '../../images/social-img1.jpeg'
+import social2 from '../../images/social-img2.jpg'
+
 export const Social = () => {
     return (
-        <div>Social</div>
+        <div className="social">
+            <div className="spacer-darkblue" />
+            <div className="container h-100">
+                <div className="row h-100">
+                    <div className="col-6 socialMedia">
+                        <h3>Join The Conversation.</h3>
+                        <div className="socialRow">
+                            <div><i class="fab fa-facebook-f"></i></div>
+                            <div style={{ background: `url(${social1})` }} />
+                            <div><i class="fab fa-twitter"></i></div>
+                        </div>
+                        <div className="socialRow">
+                            <div style={{ background: `url(${social2})` }} />
+                            <div><i class="fab fa-instagram"></i></div>
+                            <div><i class="fab fa-linkedin-in"></i></div>
+                        </div>
+                    </div>
+                    <div className="col-6 sharePrice">
+                        <h3>Investor Relations</h3>
+                        <p className="price">2,355.02p</p>
+                        <p className="change">+15.01p</p>
+                        <small>Latest share price - 06 December 2021 at 13:35</small>
+                        <button>See more investor information</button>
+                    </div>
+                </div>
+            </div>
+            <div className="spacer-lightblue" />
+        </div>
     )
 }
