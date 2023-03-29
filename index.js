e8c2aa8a8420b4588ae384c84f2886ec72e570dd
WWWarren <warrenjbyrne@gmail.com>
2023-03-29 13:29:03 +0100

    added next and prev buttons

--- index.js
+++ index.js
@@ -1,59 +1,107 @@
 function homepageBanner() {
     // DOM Helpers
-    const elNew = (tag, prop) => Object.assign(document.createElement(tag), prop);
+        const elNew = (tag, prop) => Object.assign(document.createElement(tag), prop);
 
     // Find out how many slides there are
-    const slides = document.getElementById('slides').children;
-    const slideTotal = slides.length;
+        const slides = document.getElementById('slides').children;
+        const slideTotal = slides.length;
     
     // Add buttons to slider based on total slides
-    let buttonContainer = document.getElementById('buttons');
-    const buttons = []
-    for (let i = 0; i < slideTotal; i++) {
-        const button = elNew("button", {
-            type: "button",
-            onclick: () => goto(i)
-        });
-        if (i === 0) {
-            button.classList.add("active");
+        let buttonContainer = document.getElementById('buttons');
+        const buttons = []
+        for (let i = 0; i < slideTotal; i++) {
+            const button = elNew("button", {
+                type: "button",
+                onclick: () => goto(i)
+            });
+            if (i === 0) {
+                button.classList.add("active");
+            }
+            buttons.push(button);
         }
-        buttons.push(button);
-    }
 
-    for (let i = 0; i < buttons.length; i++) {
-        buttonContainer.appendChild(buttons[i]);
-    }
+        for (let i = 0; i < buttons.length; i++) {
+            buttonContainer.appendChild(buttons[i]);
+        }
+
+    // Add arrows to slider
+        let arrows = document.getElementById('oi-slider');
+        
+        // Next arrow
+        const nextArrow = elNew("button", {
+            type: "button",
+            className: "arrow-next",
+            innerHTML: "<span></span>",
+            onclick: () => next(),
+        });
+
+        // Prev arrow
+        const prevArrow = elNew("button", {
+            type: "button",
+            className: "arrow-prev",
+            innerHTML: "<span></span>",
+            onclick: () => prev(),
+        });
+
+        arrows.append(prevArrow, nextArrow);
 
     // Animate slides when buttons are clicked
-    let x = 0;
-    const goto = (index) => {
-        x = index;
-        animateSlides(x);
-    };
-
-    const animateSlides = (slide) => {
-        // Remove active class off all slides + buttons
-        for (let i = 0; i < slideTotal; i++) {
-            slides[i].classList.remove("active");
-            buttons[i].classList.remove("active");
-        }
-
-        // Add active class onto active slide/button
-        slides[slide].classList.add("active");
-        buttons[slide].classList.add("active");
-    }
-
-    // Autoplay slider
-    const play = () => {
-        if (x === slideTotal - 1){
-            x = 0
-        } else {
-            x = x + 1
+        let x = 0;
+        const goto = (index) => {
+            x = index;
+            animateSlides(x);
         };
 
-        animateSlides(x);
-    }
+        const next = () => {
+            if (x >= slideTotal) return;
+
+            // Stop Autoplay
+            clearInterval(itv);
+
+            if (x === slideTotal - 1){
+                x = 0
+            } else {
+                x = x + 1
+            };
+            animateSlides(x);
+        }
+
+        const prev = () => {
+            if (x <= -1) return;
+
+            // Stop Autoplay
+            clearInterval(itv);
+
+            if (x === 0){
+                x = slideTotal - 1
+            } else {
+                x = x - 1
+            };
+            animateSlides(x);
+        }
+
+        const animateSlides = (slide) => {
+            // Remove active class off all slides + buttons
+            for (let i = 0; i < slideTotal; i++) {
+                slides[i].classList.remove("active");
+                buttons[i].classList.remove("active");
+            }
+
+            // Add active class onto active slide/button
+            slides[slide].classList.add("active");
+            buttons[slide].classList.add("active");
+        }
+
+    // Autoplay slider
+        const play = () => {
+            if (x === slideTotal - 1){
+                x = 0
+            } else {
+                x = x + 1
+            };
+            animateSlides(x);
+        }
 
     // Start autoplay
-    setInterval(play, 6000);
+       let itv = setInterval(play, 6000);
 }
