271b76448c146a5f59ccfb4fe4f8bc7f30b4fc00
WWWarren <warrenjbyrne@gmail.com>
2023-03-29 12:00:56 +0100

    set up js and css for slider

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-function changeSlide(e) {
-    const container = document.getElementById('container');
-    const buttons = document.getElementById('buttons'); 
+function homepageBanner() {
+    // DOM Helpers
+    const elNew = (tag, prop) => Object.assign(document.createElement(tag), prop);
 
-    // Update buttons
-    for (let b of buttons.children) {
-        b.classList.remove("active");
-        e.classList.add("active");
+    // Find out how many slides there are
+    const slides = document.getElementById('slides').children;
+    const slideTotal = slides.length;
+    
+    // Add buttons to slider based on total slides
+    let buttonContainer = document.getElementById('buttons');
+    const buttons = []
+    for (let i = 0; i < slideTotal; i++) {
+        const button = elNew("button", {
+            type: "button",
+            onclick: () => goto(i)
+        });
+        if (i === 0) {
+            button.classList.add("active");
+        }
+        buttons.push(button);
     }
 
-    // Update slides
-    for (let c of container.children) {
-      c.classList.remove("active");
-      if (c.id === e.innerHTML) {
-        c.classList.add("active");
-      }
+    for (let i = 0; i < buttons.length; i++) {
+        buttonContainer.appendChild(buttons[i]);
     }
+
+    // Animate slides when buttons are clicked
+    let x = 0;
+    const goto = (index) => {
+        x = index;
+        animateSlides(x);
+    };
+
+    const animateSlides = (slide) => {
+        // Remove active class off all slides + buttons
+        for (let i = 0; i < slideTotal; i++) {
+            slides[i].classList.remove("active");
+            buttons[i].classList.remove("active");
+        }
+
+        // Add active class onto active slide/button
+        slides[slide].classList.add("active");
+        buttons[slide].classList.add("active");
+    }
+
+    // Autoplay slider
+    const play = () => {
+        if (x === slideTotal - 1){
+            x = 0
+        } else {
+            x = x + 1
+        };
+
+        animateSlides(x);
+    }
+
+    // Start autoplay
+    setInterval(play, 6000);
 }
