c2c8c7d8753e8382825d30b019acfc1da1793a3c
WWWarren <warrenjbyrne@gmail.com>
2023-04-26 16:30:38 +0100

    refactored the tabs to a new design

--- index.js
+++ index.js
@@ -105,3 +105,34 @@ function homepageBanner() {
     // Start autoplay
        let itv = setInterval(play, 6000);
 }
+
+function oiTabs(tabs) {
+    const panels = tabs.querySelectorAll('.panel');
+    console.log(panels);
+    const titles = tabs.querySelector('.titles').children;
+    console.log(titles);
+
+    let x = 0;
+    const changeNum = (index) => {
+        x = index;
+        animatePanel(x);
+    };
+
+    const animatePanel = (panel) => {
+        // Remove active class off all panels + titles
+        for (let i = 0; i < panels.length; i++) {
+            panels[i].classList.remove("active");
+            titles[i].classList.remove("active");
+        }
+
+        // Add active class onto active panel/title
+        panels[panel].classList.add("active");
+        titles[panel].classList.remove("active");
+    }
+
+    for (let i = 0; i < titles.length; i++) {
+        titles[i].addEventListener("click", function() {
+            changeNum(i);
+        })
+    }
+}
